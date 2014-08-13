0c4d3451b561f6fe5f643ff586b1c1eb977bd183
Brendan McMahon <brendan.mcmahon@tigerpistol.com.au>
2014-08-13 11:24:29 +1000

    added nuget package configs

--- Bernos.Security/Properties/AssemblyInfo.cs
+++ Bernos.Security/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Bernos.Security")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Helper classes for security related functionality")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Brendan McMahon")]
 [assembly: AssemblyProduct("Bernos.Security")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
